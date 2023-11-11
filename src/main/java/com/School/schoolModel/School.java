183f3c204bcb8ba8f7396bae136f43c40e5a54c8
malachy <malachyomeje@gmail.com>
2023-11-11 11:40:14 +0100

    security commit

--- src/main/java/com/School/schoolModel/School.java
+++ src/main/java/com/School/schoolModel/School.java
@@ -10,7 +10,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
 
 @Data
@@ -19,7 +22,7 @@ import java.util.Date;
 @Builder
 @Entity
 
-public class School {
+public class School implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonIgnore
@@ -47,5 +50,38 @@ public class School {
     private Date date;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
